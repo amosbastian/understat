c52e1d8e5a1e55233684fa362928ce35579c5738
amosbastian <amos_bastian@hotmail.com>
2019-03-06 16:52:47 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="understat",
+    version="0.1.0",
+    packages=find_packages(),
+    description="A Python wrapper for https://understat.com/",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/amosbastian/understat",
+    author="amosbastian",
+    author_email="amosbastian@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
+    ],
+    keywords="fpl fantasy premier league understat football",
+    project_urls={
+        "Documentation": "http://fpl.readthedocs.io/en/latest/",
+        "Source": "https://github.com/amosbastian/fpl"
+    },
+    install_requires=[
+        "codecov",
+        "aiohttp",
+        "pytest-aiohttp",
+        "pytest-cov",
+        "pytest-mock",
+        "pytest",
+    ],
+)
