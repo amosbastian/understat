1779e28339953d802b700dcbf8b472a5650fa622
amosbastian <amos_bastian@hotmail.com>
2019-03-06 19:41:54 +0100

    Update tests get_results()

--- tests/test_understat.py
+++ tests/test_understat.py
@@ -66,6 +66,21 @@ class TestUnderstat(object):
         for result in results:
             assert result["isResult"]
 
+    async def test_get_results_with_options(self, loop, understat):
+        results = await understat.get_results("epl", 2018, {
+            "h": {"id": "89",
+                  "title": "Manchester United",
+                  "short_title": "MUN"}
+        })
+        assert isinstance(results, list)
+        assert len(results) > 0
+
+        results_without_option = await understat.get_results(
+            "epl", 2018, isResult=True)
+        results_with_option = await understat.get_results(
+            "epl", 2018, isResult=True)
+        assert results_with_option == results_without_option
+
     async def test_get_fixtures(self, loop, understat):
         for league in leagues:
             fixtures = await understat.get_fixtures(league, 2018)
