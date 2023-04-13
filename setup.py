5eae1e69637946f8506e8ee4c684fd3d1e4ec3c8
Amos Bastian <amosbastian@gmail.com>
2023-04-13 18:20:25 +0200

    chore: bump version to 0.1.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="understat",
-    version="0.1.8",
+    version="0.1.9",
     packages=find_packages(),
     description="A Python wrapper for https://understat.com/",
     long_description=long_description,
